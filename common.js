914535f8a5e23d1b4e15dbc49201a577d0be4b5c
Marius Aalto <108743532+aaltofar@users.noreply.github.com>
2022-11-25 18:00:21 +0100

    +

--- common.js
+++ common.js
@@ -1,25 +1,39 @@
 let arrToSort = [];
 let selectedIndex;
+let sliderVal = 200;
+let arrChecks = 0;
 populateArr();
 
 showArr();
 
-function populateArr() {
-	for (let i = 0; i < 200; i++) {
+function populateArr(amount) {
+	sliderVal = amount;
+	arrToSort = [];
+	for (let i = 0; i < amount; i++) {
 		arrToSort.push(Math.floor(Math.random() * 1000));
 	}
+	showArr();
+}
+
+function makeController() {
+	document.getElementById("controller").innerHTML = /*HTML*/ `
+	<div>${arrToSort.length}</div>
+	<input type="range" min="50" max="300" value="${sliderVal}" class="range" onchange="populateArr(this.value)">
+	<button onclick="bubbleSort()">Click to bubble-sort</button>
+	`;
 }
 
 function showArr() {
 	let html = "";
 	for (let i = 0; i < arrToSort.length; i++) {
 		html += `
-        <div style="height:${arrToSort[i]}px; ${
+        <div style="height:${arrToSort[i] / 2}px; ${
 			selectedIndex === i ? "background-color:green" : ""
 		}" id="${i}" class="bar"></div>
         `;
 	}
 	document.getElementById("app").innerHTML = html;
+	makeController();
 }
 
 function bubbleSort() {
@@ -30,13 +44,13 @@ function bubbleSort() {
 		for (let i = 0; i < len; i++) {
 			setInterval(() => {
 				if (arrToSort[i] > arrToSort[i + 1]) {
+					arrChecks++;
 					selectedIndex = arrToSort[i];
 					let tmp = arrToSort[i];
 					arrToSort[i] = arrToSort[i + 1];
 					arrToSort[i + 1] = tmp;
 					checked = true;
 					showArr();
-					console.log(i);
 				}
 			}, 20);
 		}
