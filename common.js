49d6aacda562fad485e050bdc03823dd8e9705b6
Marius Aalto <108743532+aaltofar@users.noreply.github.com>
2022-11-25 19:49:03 +0100

    +

--- common.js
+++ common.js
@@ -1,44 +1,85 @@
 let arrToSort = [];
 let selectedIndex;
-let sliderVal = 200;
 let arrChecks = 0;
+let checked = true;
+let sortAmount = 200;
 populateArr();
 
 showArr();
 
-function populateArr(amount) {
-	sliderVal = amount;
+function populateArr() {
 	arrToSort = [];
-	for (let i = 0; i < amount; i++) {
-		arrToSort.push(Math.floor(Math.random() * 1000));
+	for (let i = 0; i < sortAmount; i++) {
+		arrToSort.push(Math.floor(Math.random() * 1000) + 50);
 	}
 	showArr();
 }
 
-function makeController() {
-	document.getElementById("controller").innerHTML = /*HTML*/ `
-	<div>${arrToSort.length}</div>
-	<input type="range" min="50" max="300" value="${sliderVal}" class="range" onchange="populateArr(this.value)">
-	<button onclick="bubbleSort()">Click to bubble-sort</button>
-	`;
-}
-
-function showArr() {
-	let html = "";
+function drawArr() {
+	let graphs = "";
 	for (let i = 0; i < arrToSort.length; i++) {
-		html += `
+		graphs += `
         <div style="height:${arrToSort[i] / 2}px; ${
 			selectedIndex === i ? "background-color:green" : ""
 		}" id="${i}" class="bar"></div>
         `;
 	}
-	document.getElementById("app").innerHTML = html;
-	makeController();
+	return graphs;
 }
+
+function plus() {
+	sortAmount += 50;
+	populateArr();
+}
+function minus() {
+	sortAmount -= 50;
+	populateArr();
+}
+
+function showArr() {
+	let html = `
+	${drawArr()}
+	<div id="controller">
+	<div class="btnController">
+	<button onclick="sortAmount -= 50;populateArr()">-</button>
+	<div>${sortAmount}</div>
+	<button onclick="sortAmount += 50;populateArr()">+</button>
+	</div>
+	<button onclick="bubbleSort()">Click to bubble-sort</button>
+	<div>This sorting algorithm needed ${arrChecks} total passes to sort</div>
+	</div>
+	
+	`;
+	document.getElementById("app").innerHTML = html;
+}
+
+// function bubbleSort() {
+// 	let i, j;
+// 	let arr = arrToSort;
+// 	let len = arrToSort.length;
+
+// 	let isSwapped = false;
+
+// 	for (i = 0; i < len; i++) {
+// 		isSwapped = false;
+// 		showArr();
+// 		for (j = 0; j < len; j++) {
+// 			if (arr[j] > arr[j + 1]) {
+// 				let temp = arr[j];
+// 				arr[j] = arr[j + 1];
+// 				arr[j + 1] = temp;
+// 				isSwapped = true;
+// 				arrChecks++;
+// 			}
+// 		}
+// 		if (!isSwapped) {
+// 			break;
+// 		}
+// 	}
+// }
 
 function bubbleSort() {
 	let len = arrToSort.length;
-	let checked = false;
 	do {
 		checked = false;
 		for (let i = 0; i < len; i++) {
@@ -52,8 +93,10 @@ function bubbleSort() {
 					checked = true;
 					showArr();
 				}
-			}, 20);
+			}, 1);
 		}
 	} while (checked);
-	return arrToSort;
+	console.log("ferdig");
+	clearInterval();
+	return;
 }
